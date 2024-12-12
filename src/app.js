ae8cc0aaa4ad431736bbdd50c70daa8a6787411d
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2024-12-12 17:59:40 -0300

    Inicialización del proyecto y configuración del generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateExcuse();
 };
+
+function generateExcuse() {
+  let who = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
+  let action = ["se comió", "orinó", "aplastó", "rompió"];
+  let what = ["mi tarea", "mi teléfono", "el coche"];
+  let when = [
+    "antes de la clase",
+    "cuando estaba durmiendo",
+    "mientras estaba haciendo ejercicio",
+    "durante mi almuerzo",
+    "mientras estaba rezando"
+  ];
+
+  let excuse =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
+
+  document.getElementById("excuse").innerHTML = excuse;
+}
